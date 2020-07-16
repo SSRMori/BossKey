d8fee81583626ea51c0f0b9a04c7c80c5f2f0e71
Yining Zhang <602799027@qq.com>
2020-07-16 22:37:39 +0800

    完成1.0版本

--- README.md
+++ README.md
@@ -1,3 +1,78 @@
 # BossKey
 
-使用自定义热键，一键隐藏特定的窗口，并展示需要展示的窗口。
+![build-passing](https://img.shields.io/badge/build-passing-brightgreen)
+
+本项目基于[keyboard](https://github.com/boppreh/keyboard)和[pywin32]，**目前只支持在windows平台使用**
+
+支持使用自定义热键，一键隐藏特定的窗口，并展示需要展示的窗口。
+
+## 使用方法
+
+### 使用exe文件运行
+
+可直接在release页面下载本项目的二进制可执行文件（`.exe`），点击即可使用。
+
+### 使用python运行
+
+运行源码前，需要安装以下库：
+
+- pywin32
+- tkinter
+- keyboard
+- json
+- ctypes
+- sys
+
+使用以下命令运行程序：
+
+```shell
+cd src/
+python3 main.py
+```
+
+成功运行后，会出现如下页面:
+
+![BossKey主页面](https://raw.githubusercontent.com/SSRMori/photo/master/img/BossKey界面.png)
+
+- `confirm`按钮：选中需要**保留**的窗口后按下confirm按钮进行确认
+- `update`按钮：按下该按钮，更新当前窗口信息
+
+确认保留的页面后，使用快捷键即可对窗口进行隐藏/显示
+
+默认快捷键设置如下：
+
+|按键|功能|
+|:-:|:-|
+|`Alt+D`|隐藏未选中窗口，并置顶+最大化选中窗口|
+|`ESC+Alt+D`|显示所有（未选中/选中）窗口，可从`Alt+D`状态恢复窗口|
+|`Ctrl+Alt+Shift+H`|隐藏`BossKey`窗口|
+|`Ctrl+Alt+Shift+S`|显示`BossKey`窗口|
+
+自定义快捷键可在程序（`main.py`/`.exe`）目录下新建`hotkey.json`，并以如下格式，遵循[keyboard](https://github.com/boppreh/keyboard)hotkey的定义格式，定义**全部**的快捷键：
+
+```json
+{
+    "bossKey": "alt+d",
+    "exitBossKey": "esc+alt+d",
+    "hide": "ctrl+alt+shift+h",
+    "show": "ctrl+alt+shift+s"
+}
+```
+
+**建议使用前检查热键冲突！建议设置完成后隐藏BOSSKEY窗口！**
+
+
+## TODOs
+
+- [ ] 优化UI界面
+- [ ] 增加确认等步骤提示
+- [ ] 改进对窗口的识别
+- [ ] 跨平台支持
+
+## 打赏
+
+请开发者喝杯奶茶吧！
+
+![支付宝](https://raw.githubusercontent.com/SSRMori/photo/master/img/AlipayQR.jpg)
+
+![微信](https://raw.githubusercontent.com/SSRMori/photo/master/img/WechatQR.jpg)
--- src/main.py
+++ src/main.py
@@ -3,6 +3,22 @@ import keyboard
 import win32gui, win32con
 import functools
 import ctypes, sys
+import json
+
+windowList = []
+selectList = []
+selectedList = []
+win = None
+checkButtons = []
+confirmButton = None
+updateButton = None
+# showAll = None
+log_file = 'bossKey.log'
+
+def write_log(string):
+    f = open(log_file, "a+")
+    f.write(string)
+    f.write('\n')
 
 def is_admin():
     try:
@@ -10,55 +26,128 @@ def is_admin():
     except:
         return False
 
-
 def addWindow(window, windowList):
     if (win32gui.IsWindow(window) 
     and win32gui.IsWindowEnabled(window) 
     and win32gui.IsWindowVisible(window) 
     and win32gui.GetWindowText(window) != ""):
+        write_log('Add window ' + str(window) + ' to windowList')
         windowList.append(window)
 
-
 def get_all_windows():
+    write_log('Start get all windows')
     windowList = []
     win32gui.EnumWindows(addWindow, windowList)
     return windowList
 
-windowList = get_all_windows()
-selectList = []
-selectedList = []
-win = tkinter.Tk()
-
 def get_checked_window():
+    write_log('Start get seleted windows')
     global selectedList
+    global windowList
     selectedList = []
     for i in range(len(selectList)):
         if selectList[i].get() == 1:
+            write_log('window ' + str(windowList[i]) + " is selected")
             selectedList.append(windowList[i])
     # print(selectedList)
+
+def init_window():
+    global win
+    global windowList
+    global checkButtons
+    global confirmButton
+    global updateButton
+    global selectList
+    global selectedList
+    selectList = []
+    selectedList = []
+    windowList = []
+    # global showAll
+    write_log("init window")
+    windowList = get_all_windows()
+    for i in range(len(windowList)):
+        tempVar = tkinter.IntVar()
+        checkButtons.append(tkinter.Checkbutton(win, text=win32gui.GetWindowText(windowList[i]), variable=tempVar))
+        # print(len(checkButtons))
+        checkButtons[len(checkButtons)-1].grid(row=i, sticky=tkinter.W)
+        selectList.append(tempVar)
+    confirmButton = tkinter.Button(win, text="Confirm", command=get_checked_window)
+    confirmButton.grid(row=len(windowList) + 1)
+    updateButton = tkinter.Button(win, text="update", command=update_window)
+    updateButton.grid(row=len(windowList) + 2)
+    # showAll = tkinter.Button(win, text="showAll", command=showAllWindow)
+    # showAll.grid(row=len(windowList) + 3)
+
+def exit_boss_key():
+    global windowList
+    for window in windowList:
+        win32gui.ShowWindow(window, win32con.SW_SHOW)
+        write_log("show window " + str(window))
+        # win32gui.ShowWindow(window, win32con.SW_MAXIMIZE)
+        # win32gui.SetForegroundWindow(window)
+
+def update_window():
+    write_log("update window")
+    global checkButtons
+    global confirmButton
+    global updateButton
+    # global showAll
+    for button in checkButtons:
+        button.destroy()
+    checkButtons = []
+    confirmButton.destroy()
+    confirmButton = None
+    updateButton.destroy()
+    updateButton = None
+    # showAll.destroy()
+    # showAll = None
+    init_window()
+
 
 def boss_key():
     # print("boss_key")
     global selectedList
     currentWindowList = get_all_windows()
     for window in currentWindowList:
-        win32gui.ShowWindow(window, win32con.SW_MINIMIZE)
+        win32gui.ShowWindow(window, win32con.SW_HIDE)
+        write_log("hide window " + str(window))
     for window in selectedList:
+        write_log("show window " + str(window))
         # print(window)
+        win32gui.ShowWindow(window, win32con.SW_SHOW)
         win32gui.ShowWindow(window, win32con.SW_MAXIMIZE)
         win32gui.SetForegroundWindow(window)
 
+def hideWindow():
+    # win.iconify()
+    global win
+    win.withdraw()
+
+def showWindow():
+    global win
+    win.deiconify()
+
 if __name__ == "__main__":
-    if is_admin():
+    if is_admin():# or True:
     # Code of your program here
-        keyboard.add_hotkey('alt+d', boss_key)
+        win = tkinter.Tk()
+        win.iconbitmap('./../image/icon.ico')
         win.title("BOSSKEY")
-        win.iconify()
-        for i in range(len(windowList)):
-            tempVar = tkinter.IntVar()
-            tkinter.Checkbutton(win, text=win32gui.GetWindowText(windowList[i]), variable=tempVar).grid(row=i, sticky=tkinter.W)
-            selectList.append(tempVar)
-        tkinter.Button(win, text="Confirm", command=get_checked_window).grid(row=len(windowList) + 1)
+        f = open('hotkey.json')
+        if f is None:
+            keyboard.add_hotkey('alt+d', boss_key)
+            keyboard.add_hotkey('esc+alt+d', exit_boss_key)
+            keyboard.add_hotkey('ctrl+alt+shift+h', hideWindow)
+            keyboard.add_hotkey('ctrl+alt+shift+s', showWindow)
+        else:
+            settings = json.load(f)
+            # print(settings)
+            keyboard.add_hotkey(settings['bossKey'], boss_key)
+            keyboard.add_hotkey(settings['exitBossKey'], exit_boss_key)
+            keyboard.add_hotkey(settings['hide'], hideWindow)
+            keyboard.add_hotkey(settings['show'], showWindow)
+        # win.iconify()
+        init_window()
         win.mainloop()
     else:
     # Re-run the program with admin rights
